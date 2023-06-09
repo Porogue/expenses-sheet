c67de9310fdae0adb475c8dd9157a1712d098018
Porogue <pabloorozcoguerrero@gmail.com>
2023-06-09 07:48:03 +0200

    Defining methods

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from dbconfig import dbconf
 
 connection = psy.connect(**dbconf)
 
+cursor = connection.cursor()
+
 print(connection)
 
-cursor = connection.cursor()
+class ExpensesSheet:
+    def __init__(self):
+        self.connection = psy.connect(**dbconf)
+        self.cursor = connection.cursor()
+        print("You have connected to the database")
+    
+    def __close__(self):
+        self.cursor.close()
+        print("Connection to the database has been closed")
+    
+    def selectAll(self):
+        self
